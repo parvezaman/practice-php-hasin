22da1462f4080c7ca65c126090502bdff9010a63
parvezaman <emailtoamanullah@gmail.com>
2023-05-25 07:53:12 +0600

    playing around with printf and sprintf

--- 1. basics/1.4 print output.php
+++ 1. basics/1.4 print output.php
@@ -35,4 +35,37 @@ echo "You
 also 
 can 
 use line \n
-break!";
+break!\n";
+
+
+// lets play a little more with printf
+
+$fname ="pinak";
+$lname = 'lowey';
+
+printf("His name is %s %s \n", $fname, $lname);
+printf('His name is %2$s %1$s', $fname, $lname); // watch out the single quote (' ')
+echo "\n";
+// see! now you cant use \n with it. so to use the escaping we are going to use "" as following
+printf("His name is %2\$s %1\$s \n", $fname, $lname); // watch out the single quote (' ')
+
+$myFloat = 5.1564285;
+// lets print upto 2 decimal
+printf("%.2f \n", $myFloat);
+printf("%04d \n", $myFloat);
+printf("%07.2f \n", $myFloat);
+
+//  another way of line 56 using sprintf
+// notable thing regarding sprintf is, by sprintf we can get return of the print. printf function only prints the output but sprintf gives us return of the print and we can store them into another variable which can be used later on
+$formattedFloat = sprintf("%07.2f", $myFloat);
+echo $formattedFloat . "\n";
+/*
+In this code, the format specifier "%07.2f" is used with sprintf. Let's break it down:
+
+% indicates the start of a format specifier.
+0 is the padding character. It specifies that if the resulting string is shorter than the specified width, it should be padded with zeros.
+7 is the width of the resulting string. It includes both the integer part and the decimal part of the number, as well as the decimal point.
+.2 specifies the precision, indicating that you want to display two decimal places after the decimal point.
+f represents the floating-point format.
+The sprintf function formats the value of $myFloat according to the specified format specifier and stores the result in the $formattedFloat variable. The resulting value will be "0005.16".
+*/
--- 1. basics/1.9 number system.php
+++ 1. basics/1.9 number system.php
@@ -6,6 +6,7 @@ $hexNum = 0x1B;
 $binNum = 0b1010110;
 
 printf("decimal eqvalent of octal %o = decimal %d\n", $octalNum, $octalNum);
+printf("decimal eqvalent of octal %1\$o = decimal %1\$d\n", $octalNum); // same thing but using one parameter
 printf("decimal eqvalent of hexadecimal %x = decimal %d\n", $hexNum, $hexNum);
 printf("decimal eqvalent of hexadecimal (upper case) %X = decimal %d\n", $hexNum, $hexNum);
 printf("decimal eqvalent of binary %b = decimal %d\n", $binNum, $binNum);
