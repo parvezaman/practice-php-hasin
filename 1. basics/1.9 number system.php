fd97d7ac73179a897a046e1ab6a2bfca08fb8563
parvezaman <emailtoamanullah@gmail.com>
2023-05-25 07:22:13 +0600

    number system declaration and conversion

---
+++ 1. basics/1.9 number system.php
@@ -0,0 +1,11 @@
+<?php
+// following are the metods of defining different number system in php
+$decinalNum = 12;
+$octalNum = 012;
+$hexNum = 0x1B;
+$binNum = 0b1010110;
+
+printf("decimal eqvalent of octal %o = decimal %d\n", $octalNum, $octalNum);
+printf("decimal eqvalent of hexadecimal %x = decimal %d\n", $hexNum, $hexNum);
+printf("decimal eqvalent of hexadecimal (upper case) %X = decimal %d\n", $hexNum, $hexNum);
+printf("decimal eqvalent of binary %b = decimal %d\n", $binNum, $binNum);
