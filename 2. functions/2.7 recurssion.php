083c77e27b740eb07cac22fc4fad4c079f1d334a
parvezaman <emailtoamanullah@gmail.com>
2023-06-26 09:04:31 +0600

    recursive in php

---
+++ 2. functions/2.7 recurssion.php
@@ -0,0 +1,51 @@
+<?php
+// print 1 - 10 with recurrsion
+
+/* function printNum($i)
+{
+    if ($i>10) {
+        return;
+    }
+
+    echo "$i \n";
+    $i++;
+    printNum($i);
+}
+
+printNum(1); */
+
+
+/* function printNum($counter, $end, $stepping=1)
+{
+    if ($counter>$end) {
+        return;
+    }
+    echo "$counter \n";
+    $counter += $stepping;
+    printNum($counter, $end, $stepping);
+}
+
+printNum(1, 10, 3);
+ */
+
+//  Find fibonacci series
+// 0 1 1 2 3 5 8 13 21 34
+function fibonacci($old, $new, $end)
+{
+    static $start;
+    $start = $start ?? 1;
+    if ($start > $end) {
+        return;
+    }
+    $start++;
+
+    echo "$old ";
+
+    $_temp = $old+$new;
+    $old = $new;
+    $new = $_temp;
+
+    fibonacci($old, $new, $end);
+}
+
+fibonacci(0, 1, 10);
